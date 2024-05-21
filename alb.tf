76461c76a83bd81ae087327a4f603717db47bf86
saurelle <msmkamdem@gmail.com>
2024-05-21 13:07:52 -0500

    terraform

--- alb.tf
+++ alb.tf
@@ -1,3 +1,4 @@
+# Create a target group
 resource "aws_lb_target_group" "alb-target-group" {
   name     = "application-lb-tg"
   port     = 80
@@ -16,6 +17,7 @@ resource "aws_lb_target_group" "alb-target-group" {
     unhealthy_threshold = 3
   }
 }
+# Attach the target group to the AWS instances
 resource "aws_lb_target_group_attachment" "attach-app" {
   target_group_arn = aws_lb_target_group.alb-target-group.arn 
   target_id        = aws_instance.server1.id 
@@ -26,9 +28,11 @@ resource "aws_lb_target_group_attachment" "attach-app2" {
   target_id        = aws_instance.server2.id 
   port             = 80
 }
+##Listenner
+
 resource "aws_lb_listener" "alb-http-listener" {
     load_balancer_arn = aws_lb.application-lb.arn
-    port              = "80"
+    port              = 80
     protocol          = "HTTP"
   
     default_action {
@@ -36,6 +40,8 @@ resource "aws_lb_listener" "alb-http-listener" {
       target_group_arn = aws_lb_target_group.alb-target-group.arn
     }
   }
+
+   #Create the load balancer
 resource "aws_lb" "application-lb" {
   name               = "application-lb"
   internal           = false
@@ -51,3 +57,4 @@ resource "aws_lb" "application-lb" {
     
   }
 }
+
